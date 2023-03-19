c2d2d0c96092c3a1528bbcf78b82a1ba23686f92
JangKroed <didlsdydgh@gmail.com>
2023-03-19 23:33:57 +0900

    users  GET relations update / accesstoken name change authorization

--- src/db/repositories/users.ts
+++ src/db/repositories/users.ts
@@ -10,49 +10,37 @@ class UsersRepositories extends Users {
   };
 
   findOneId = async (userId: number) => {
-    return await Users.findOne({ where: { userId } });
+    return await Users.findOne({
+      where: { userId },
+      relations: { myhome: true },
+    });
   };
 
   findOneEmail = async (email: string) => {
-    return await Users.findOne({ where: { email } });
+    return await Users.findOne({
+      where: { email },
+      relations: { myhome: true },
+    });
   };
-
-  // findById = async (userId: number, email: string) => {
-  //   const findById = await Users.findByPk({
-  //     where: {
-  //       [Op.and]: [{ userId }, { email }],
-  //     },
-  //   });
-  //   return findById;
-  // };
-
-  // updateRefresh = async (refreshToken: string, user: Users) => {
-  //   await Users.update({ refreshToken }, { where: { userId: user.userId } });
-  // };
-
-  // ----------------------------------------------------------------
 
   findMaxUser = async () => {
     const users = await Users.find();
     const userMaxId = users[users.length - 1].userId;
     return await Users.findOne({
       where: { userId: userMaxId },
-      // order: [['userId', 'desc']],
     });
   };
 
   findByUser = async (userId: number) => {
     return Users.findOne({
-      // attributes: {
-      //   exclude: ['password'],
-      // },
       where: { userId },
+      relations: { myhome: true },
     });
   };
 
   findAllUsers = async () => {
-    return Users.find();
-  }
+    return Users.find({ relations: { myhome: true } });
+  };
 
   // chargeDotori = async (userId, price) => {
   //   const isDotori = await this.findByUser(userId);
